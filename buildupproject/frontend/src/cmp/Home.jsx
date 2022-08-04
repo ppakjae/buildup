7dd489d4eb9d83cd8e3d054c985950be7a553b3d
jaqwe2301 <jaqwe2301@naver.com>
2022-08-04 23:40:00 +0900

    20220804v1

--- buildupproject/frontend/src/cmp/Home.jsx
+++ buildupproject/frontend/src/cmp/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { HomeDiv, MainButton } from "../styledComponents";
 import { useNavigate } from "react-router-dom";
+import "../index.css"
 
 const Home = () => {
   const navigate = useNavigate();
@@ -13,8 +14,8 @@ const Home = () => {
       <div className="home_wrapper">
         <img src="/img/logo.png" alt="buildup_logo.png" />
         <div className="dsc_wrapper">
-          <h1>빌덥</h1>
-          <p>
+          <h1 className="score600">빌덥</h1>
+          <p className="score500">
             빌덥은 대학생을 대상으로 팀 빌딩과 외주가 가능한 커뮤니티 플랫폼입니다.
             <br />
             전문 기술 및 금전 등의 부족으로 외주를 맡기려는 대학생과 포트폴리오를
@@ -24,7 +25,7 @@ const Home = () => {
             빌덥과 함께 대학 생활의 질을 높여볼까요?
           </p>
         </div>
-        <MainButton onClick={goTeamBuild} type="button" fontSize="17px" style={{ marginTop: "100px" }}>
+        <MainButton onClick={goTeamBuild} type="button" fontSize="17px" className="score500" style={{ marginTop: "100px" }}>
           시작하기!
         </MainButton>
       </div>
