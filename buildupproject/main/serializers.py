0f75e6d97a0b8fde6aa238d846fa8af7f25a3113
Jaesun Park <41979078+ppakjae@users.noreply.github.com>
2022-08-15 14:27:50 +0900

    member restful api

--- buildupproject/main/serializers.py
+++ buildupproject/main/serializers.py
@@ -1,12 +1,19 @@
+from dataclasses import field
 from pyexpat import model
 from rest_framework import serializers
-from .models import Post
+from .models import Member, Post
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
+        model = Post
         fields = (
             'id',
             'title',
             'content'
         )
-        model = Post
+        
+
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Member
+        fields = '__all__'
--- buildupproject/main/urls.py
+++ buildupproject/main/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-
-from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import MemberList, MemberDetail
 
 urlpatterns = [
-    path("", views.ListPost.as_view()),
-    path("int:pk", views.DetailPost.as_view()),
+    # path("", views.ListPost.as_view()),
+    # path("<int:pk>", views.DetailPost.as_view()),
+
+    path("member", MemberList.as_view()),
+    path("member/<int:pk>", MemberDetail.as_view()),
 
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
