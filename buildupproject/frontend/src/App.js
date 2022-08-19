56ebeddc6966b56ee10402c287a9b4da5750a26e
jeongwoo903 <www8565@naver.com>
2022-08-19 12:11:34 +0900

    real finish

--- buildupproject/frontend/src/App.js
+++ buildupproject/frontend/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './cmp/Home';
 import TeamBuild from './cmp/TeamBuild';
+import WhoYou from './cmp/WhoYou';
+import Competition from './cmp/Competition';
 import FindMember from './cmp/FindMember';
 import SelectCondition from './cmp/SelectCondition';
 import SimilarMember from './cmp/SimilarMember';
 import More from './cmp/More';
-import RestAPI from "./RestAPI.js"
+import CreatingTeam from './cmp/CreatingTeam';
+
+import RestAPI from "./RestAPI.js";
 
 
 const App = () => {
@@ -15,6 +19,9 @@ const App = () => {
             {/* path는 _ 로 구분했습니다! */}
             <Route path='/' element={<Home />} />
             <Route path='/team_build' element={<TeamBuild />} />
+            <Route path="/who_are_you" element={<WhoYou />} />
+            <Route path='/competition' element={<Competition />} />
+            <Route path="/creating_team" element={<CreatingTeam />} />
             <Route path="/find_member" element={<FindMember />} />
             <Route path="/select_condition" element={<SelectCondition />} />
             <Route path="/similar_member" element={<SimilarMember />} />
